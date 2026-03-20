77947460a719b4dbf4adc20215560b2986a92c33
Chaz Cornwall <chazcornwall@gmail.com>
2026-03-20 17:35:53 -0400

    updated example

--- example_solver.py
+++ example_solver.py
@@ -2,9 +2,11 @@ from amazeing_challenge.contracts import Solver, Maze
 
 from utils.graph_search import dijkstras
 
+from typing import Iterator, List
+
 class ExampleSolver(Solver):
 
-    def solve(self, maze : Maze, start, goal) -> list:
+    def solve(self, maze : Maze, start, goal) -> Iterator[List]:
         costCb = lambda pose1, pose2 : 1.0 # All edges are feasible neighbors. Therefore, all edges have a cost of 1.0.
         neighborsCb = lambda pose : [edge[1] for edge in maze.get_edges(pose)] # Neighbors are only the last element of edges
-        return dijkstras(start, goal, neighborsCb, costCb, verbose=False)
+        yield from dijkstras(start, goal, neighborsCb, costCb, verbose=False)
